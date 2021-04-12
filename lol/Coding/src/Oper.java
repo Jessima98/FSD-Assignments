bcb4a9e4767801463280c9b0ce9c138f1c99e8b1
Jessima <jessimajohn@gmail.com>
2021-04-12 11:41:14 +0530

    Java

---
+++ lol/Coding/src/Oper.java
@@ -0,0 +1,20 @@
+
+public class Oper {
+
+
+	
+	static int c=20;
+
+public static void main(String[] args)
+{
+	int a=2;
+	System.out.println(a++);
+	System.out.println(a);
+	System.out.println(++a);
+	System.out.println(a);
+	System.out.println(a++);
+	System.out.println(a++);
+	
+	
+}
+}
